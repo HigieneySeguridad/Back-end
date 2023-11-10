abb14389d5cd5bb5afec874186f3ccaa345ad9d6
nikoveron <nicoveron28@gmail.com>
2023-11-09 23:36:02 -0300

    username edit

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -24,11 +24,40 @@ const iniciarSesion = async (req, res) => {
 
     console.log("Inicio de Sesión correcto!");
 
-    res.json({ token, role: usuario.role, nombre: usuario.username });
+    res.json({ token, role: usuario.role, nombre: usuario.username, userId: usuario._id  });
   } catch (error) {
     console.error('Error de inicio de sesión:', error);
     res.status(500).json({ message: 'Error interno del servidor' });
   }
 };
 
-  module.exports = {iniciarSesion}
+const editarPerfil = async (req, res) => {
+  const { userId } = req.params;
+  const { nombre } = req.body; // Obtener el ID de usuario de los parámetros
+
+  try {
+    const usuario = await Usuario.findById(userId);
+
+    if (!usuario) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    // Obtener los campos a actualizar desde el cuerpo de la solicitud
+    
+
+    // Actualizar los campos del usuario
+    usuario.username = nombre;
+    // Actualiza otros campos según tus necesidades
+
+    // Guarda los cambios en la base de datos
+    await usuario.save();
+
+    res.json({ message: 'Perfil actualizado correctamente' });
+  } catch (error) {
+    console.error('Error al editar el perfil del usuario:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+};
+
+
+module.exports = {iniciarSesion,editarPerfil}
