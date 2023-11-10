b0b6c61962efeb409981763b5144f4ca4e415695
nikoveron <nicoveron28@gmail.com>
2023-11-09 23:34:16 -0300

    edit username

--- routes/loginUser.js
+++ routes/loginUser.js
@@ -1,8 +1,9 @@
-const { iniciarSesion } = require("../controllers/loginController");
+const { iniciarSesion,editarPerfil } = require("../controllers/loginController");
 const express = require("express")
 const Router = express.Router();
 
 
 Router.post('/', iniciarSesion);
+Router.put('/:userId', editarPerfil);
 
 module.exports = Router;
