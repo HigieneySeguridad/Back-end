8a2780d3faa8bca81216b23b9c5dde2e96e7d121
nico <nicoveron28@gmail.com>
2023-11-15 10:25:41 -0300

    editar mobile

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -28,8 +28,32 @@ const iniciarSesion = async (req, res) => {
   } catch (error) {
     console.error('Error de inicio de sesión:', error);
     res.status(500).json({ message: 'Error interno del servidor' });
+
   }
 };
 
+const editarPerfil = async (req, res) => {
+  const { userId } = req.params;
+  const { telefono, dni } = req.body;
 
-module.exports = {iniciarSesion}
+  try {
+    const usuario = await Usuario.findById(userId);
+
+    if (!usuario) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    // No actualices el nombre de usuario
+    usuario.telefono = telefono;
+    usuario.dni = dni;
+
+    await usuario.save();
+
+    res.json({ message: 'Perfil actualizado correctamente' });
+  } catch (error) {
+    console.error('Error al editar el perfil del usuario:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+};
+
+module.exports = {iniciarSesion,editarPerfil}
